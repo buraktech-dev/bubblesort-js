b8dffa0ea53989c7599b8a12cdcd1242b5332c8f
Burak Tech <buraktechdev@gmail.com>
2024-02-12 20:10:02 +0100

    bubble sort algorithm in javascript

---
+++ bubblesort.js
@@ -0,0 +1,16 @@
+// Bubble Sort in JavaScript
+
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length-1; i++) {
+        for (let j = 0; j < arr.length-i; j++) {
+            if (arr[j] < arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+            }
+        }
+    }
+
+    return arr;
+}
+
+console.log(bubbleSort([2,1,3,5,6,4]))
